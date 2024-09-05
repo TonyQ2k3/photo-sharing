28f605647b6f2424f9087407f85b998792eeeaf1
TonyQ2k3 <ntbquoc2015@gmail.com>
2024-09-05 23:51:28 +0700

    Add authentication module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule, PostsModule, PrismaModule],
+  imports: [
+    UsersModule, 
+    PostsModule, 
+    PrismaModule, 
+    AuthModule,
+    JwtModule.register({
+      secret: 'yoursecretkey', // Change this to a secure key
+      signOptions: { expiresIn: '60s' },
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,9 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
-    
+    @Post('register')
+    async registerUser(@Body() body: { email: string, username: string, password: string }) {
+        return this.usersService.createUser(body);
+    }
 }
