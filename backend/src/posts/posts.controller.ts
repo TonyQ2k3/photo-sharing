c24a1fcdaa4903d8fd487045b812be274ca546bb
TonyQ2k3 <ntbquoc2015@gmail.com>
2024-09-09 10:53:48 +0700

    Added Post handler

--- backend/src/posts/posts.controller.ts
+++ backend/src/posts/posts.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { PostsService } from './posts.service';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+    constructor(private postsService: PostsService) {}
+
+    @Get('all')
+    async getPosts() {
+        return this.postsService.getPosts();
+    }
+    @Get('all/:id')
+    async getPostById(@Param('id') id: string) {
+        let postId = parseInt(id);
+        return this.postsService.getPostById(postId);
+    }
+    @Get('user/:userId')
+    async getPostByUserId(@Param('userId') userId: string) {
+        let id = parseInt(userId);
+        return this.postsService.getPostByUser(id);
+    }
+}
--- backend/src/posts/posts.module.ts
+++ backend/src/posts/posts.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
   
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [PostsController],
   providers: [PostsService]
 })
