4bf9c8024d5bd2f7f16f561be6fd70a5f572b0b5
TonyQ2k3 <ntbquoc2015@gmail.com>
2024-10-03 15:14:57 +0700

    Update Header with auth

--- frontend/app/components/Header.jsx
+++ frontend/app/components/Header.jsx
@@ -1,13 +1,29 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import {add, heart, home, account} from '@/public'
 import IconWithTooltip from './IconWithTooltip'
 
+import { auth } from '@/lib/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+
+
 function Header() {
-  return (
+    const [user, setUser] = React.useState(null);
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            } else {
+                setUser(null);
+            }
+        });
+    }, []);
+
+    return (
     <div className="w-full border-b-2 border-gray-300">
         <div className='flex justify-between items-center w-full lg:w-3/4 mx-auto py-2'>
             <Link href="/" className="font-merienda font-semibold text-lg md:text-2xl mx-2">PixShare</Link>
@@ -22,9 +38,7 @@ function Header() {
                 <Link href="#" className="nav-wrapper">
                     <IconWithTooltip src={heart} alt="Favorites" tooltip="Favorites" />
                 </Link>
-                <Link href="/login">
-                    <UserIcon />
-                </Link>
+                <UserIcon user={user} />
             </nav>
         </div>
     </div>
@@ -39,7 +53,7 @@ function SearchBar() {
     )
 }
 
-function UserIcon() {
+function UserIcon({ user }) {
     const [isOpen, setIsOpen] = React.useState(false);
 
     const toggleDropdown = () => {
@@ -48,20 +62,29 @@ function UserIcon() {
 
     return (
     <div className="mx-[5px] md:mx-2 p-[5px] relative">
-        {/* <div onClick={toggleDropdown} className="">
-            <Image src={account} alt="User" className="nav-icon"/>
-        </div>
+        {
+            user ? (
+                <div>
+                    <div onClick={toggleDropdown} className="relative group">
+                        <Image src={account} alt="User" className="nav-icon"/>
+                        <div className="tooltip-text">
+                            <p>{user.displayName}</p>
+                        </div>
+                    </div>
 
-        {isOpen && (
-        <div className='dropdown-bar'>
-            <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-                <Link href="/profile" className='dropdown-item'>Profile</Link>
-                <Link href="/settings" className='dropdown-item'>Settings</Link>
-                <Link href="/logout" className='dropdown-item'>Log Out</Link>
-            </div>
-        </div>
-        )} */}
-        <p className='hover:text-gray-500'>Log In</p>
+                    {isOpen && (
+                    <div className='dropdown-bar'>
+                        <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                            <Link href="/profile" className='dropdown-item'>Profile</Link>
+                            <Link href="/settings" className='dropdown-item'>Settings</Link>
+                            <Link href="/logout" className='dropdown-item'>Log Out</Link>
+                        </div>
+                    </div>)}
+                </div>
+            ) : (
+                <Link href="/login" className='hover:text-gray-500'>Log in</Link>
+            )
+        }
     </div>
     );
 }
