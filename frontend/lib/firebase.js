3e87c2d3df6bb0c1948d690aff24209b942cc988
TonyQ2k3 <ntbquoc2015@gmail.com>
2024-10-04 23:08:17 +0700

    Added Firebase Storage and Firestore

--- frontend/lib/firebase.js
+++ frontend/lib/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 // Firebase configuration
 const firebaseConfig = {
@@ -14,6 +16,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
 
 // Initialize Firebase Authentication and get a reference to the service
-export { auth };
+export { auth, db, storage };
